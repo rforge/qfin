268f95cffddbb02e39e03a734495dabad86916f4
phofmarc <phofmarc@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-11-24 15:14:03 +0000

    eda.shape
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/qfin@5 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/Rsafd/R/eda.shape.R
+++ pkg/Rsafd/R/eda.shape.R
@@ -1,6 +1,7 @@
 `eda.shape` <-
 function(x)
 {   oop <- par(mfrow = c(2, 2))
+    on.exit(par(oop))
     hist(x, probability=TRUE, main="Histogram", xlab="")
     boxplot(x, main ="Boxplot")
     iqd <- IQR(x)
@@ -8,6 +9,6 @@ function(x)
         type = "l", main="Density")
     qqnorm(x)
     qqline(x)
-    on.exit(par(oop))
+    
 }
 
